0f948c1740bd0d94b2e908437ae7d382bf881816
uras2016 <uras@i.ua>
2016-04-15 21:24:41 +0300

    add method WrongNumberException()

--- src/main/java/goit/group3/WrongNumberException.java
+++ src/main/java/goit/group3/WrongNumberException.java
@@ -1,7 +1,15 @@
 package main.java.goit.group3;
 
 public class WrongNumberException {
-    public WrongNumberException(String number) {
+    public static boolean WrongNumberException(String number) {
+
+         {
+            int extraChars = number.length() - 16;
+            if (extraChars != 0) {
+                throw new IllegalArgumentException("Number length should be 16 characters!");
+            }
+            return true;
+        }
 
     }
 }
